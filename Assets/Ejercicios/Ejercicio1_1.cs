d19aaaf353f1246e7968c1a2ca37f1df5f2b73e8
Axeltaylor36 <alejandrosastre36@gmail.com>
2024-09-11 18:20:25 +0200

    He añadido lo mismo q el profe en clase

--- Assets/Ejercicios/Ejercicio1_1.cs
+++ Assets/Ejercicios/Ejercicio1_1.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Ejercicio_1 : MonoBehaviour
 {
+    string nombrePersonaje = "Roger";
+    bool veneno = false;
+    float porcentajeAcierto =43.7f;
+    char solucionPuzzle = 'C';
+    int resultado = 3;
+    int danhoEnemigo = 20;
+    int pocion = 15;
+    
+    
+    int danho = 37;
+    int vidas = 100;
     // Start is called before the first frame update
     void Start()
     {
-        
+        vidas = vidas / danho;
+        Debug.Log("Hola " + nombrePersonaje + "bienvenido a mi mundo");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("Adiós mundo:");
     }
 }
