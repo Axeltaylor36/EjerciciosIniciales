3f2c218307e5f1f372f93815a352a5dd919db594
Axeltaylor36 <alejandrosastre36@gmail.com>
2024-09-15 18:24:53 +0200

    Update Ejercicio2_8.cs

--- Assets/Ejercicios/Ejercicio2_8.cs
+++ Assets/Ejercicios/Ejercicio2_8.cs
@@ -7,11 +7,20 @@ using UnityEngine.Video;
 public class Ejercicio2_8 : MonoBehaviour
 {
     float vidaTotal = 120f;
-    float vidaRestante = 120;
+    
 
     void Start()
     {
-        
+        float turno1 = vidaTotal - (120 * 3 / 100);
+            Debug.Log("Ahora tienes " + turno1);
+        float turno2 = vidaTotal - (turno1 * 6 / 100);
+            Debug.Log("Ahora tienes " + turno2);
+        float turno3 = vidaTotal - (turno2 * 9 / 100);
+            Debug.Log("Ahora tienes " + turno3);
+        float turno4 = vidaTotal - (turno3 * 12 / 100);
+            Debug.Log("Ahora tienes " + turno4);
+        float turno5 = vidaTotal - (turno4 * 15 / 100);
+            Debug.Log("Ahora tienes " + turno5);
     }
 
     void Update()
