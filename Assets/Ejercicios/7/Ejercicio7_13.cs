7fa8ad3d4d9e12ce9fd9753c8793ea1c38d0131f
Axeltaylor36 <alejandrosastre36@gmail.com>
2024-09-29 17:29:35 +0200

    7.13

--- Assets/Ejercicios/7/Ejercicio7_13.cs
+++ Assets/Ejercicios/7/Ejercicio7_13.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Ejercicio7_13 : MonoBehaviour
 {
+    [SerializeField] private int num;
     // Start is called before the first frame update
     void Start()
     {
+        bool Primo = Primo2(num);
+        if (Primo)
+        {
+            Debug.Log(num + "  es primo.");
+        }
+        else  
+        Debug.Log(num + "  no es primo.");
         
     }
-
-    // Update is called once per frame
-    void Update()
+    bool Primo2(int num1)
     {
-        
+        if (num1 <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= num1 / 2; i++)
+        {
+            if (num1 % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
